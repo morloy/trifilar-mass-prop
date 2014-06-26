c4b823868239c50c60b6cb9f26fb5afbd1e63c73
Timo Horstschäfer <mail@timo-horstschaefer.de>
2014-06-26 16:06:49 +0200

    Pretty print uncertainties.

--- inc/tools.py
+++ inc/tools.py
@@ -20,7 +20,7 @@ def StatPrint(name, D, unit=''):
 	perc = "({:.2f} %)".format(abs(s/m*100))
 	if unit <> '':
 		unit = '[{}]'.format(unit)
-	print "{:<8} {:>17} +/- {:<17} {:>10} {:<10} {:<30}".format(name+':', m, s, perc, unit, D)
+	print "{:<8} {:>17} {:>10} {:<10} {:<30}".format(name+':', un2str(m, s), perc, unit, D)
 
 def Savefig(fig, name):
 	out = "out"
@@ -32,3 +32,48 @@ def Savefig(fig, name):
 		os.mkdirs(directory)
 
 	fig.savefig("{}/{}/{}.pdf".format(out, script, name_filtered))
+
+from math import floor, log10
+
+# uncertainty to string, adapted from
+# http://stackoverflow.com/questions/6671053/python-pretty-print-errorbars
+def un2str(x, xe, precision=2):
+    """
+	pretty print nominal value and uncertainty
+
+    x  - nominal value
+    xe - uncertainty
+    precision - number of significant digits in uncertainty
+
+    returns shortest string representation of `x +- xe` either as
+        x.xx(ee)e+xx
+    or as
+        xxx.xx(ee)
+	"""
+    # base 10 exponents
+    x_exp = int(floor(log10(abs(x))))
+    xe_exp = int(floor(log10(xe)))
+
+    # uncertainty
+    un_exp = xe_exp-precision+1
+    un_int = round(xe*10**(-un_exp))
+
+    # nominal value
+    no_exp = un_exp
+    no_int = round(x*10**(-no_exp))
+
+    # format - nom(unc)exp
+    fieldw = x_exp - no_exp
+    fmt = '%%.%df' % fieldw
+    result1 = (fmt + '(%.0f)e%d') % (no_int*10**(-fieldw), un_int, x_exp)
+
+    # format - nom(unc)
+    fieldw = max(0, -no_exp)
+    fmt = '%%.%df' % fieldw
+    result2 = (fmt + '(%.0f)') % (no_int*10**no_exp, un_int*10**max(0, un_exp))
+
+    # return shortest representation
+    if len(result2) <= len(result1):
+        return result2
+    else:
+        return result1
